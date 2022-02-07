0d0c12d7b352f8fdbc3f68bc1f1db3c408048b22
Roberto B <roberto.butti@gmail.com>
2022-02-07 19:22:02 +0100

    getStaticProps , with json props

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import CallToAction from '../components/CallToAction'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+function Home({ctas}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -22,33 +23,12 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          {ctas.map((cta, i ) => (
+            <CallToAction key={i}
+            {...cta}
+            />
+          ))}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
@@ -67,3 +47,42 @@ export default function Home() {
     </div>
   )
 }
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export async function getStaticProps() {
+
+  const ctas = [
+    {
+      url: "https://nextjs.org/docs",
+      title: "Documentation",
+      description: "Find in-depth information about Next.js features and API."
+    },
+    {
+      url: "https://nextjs.org/learn",
+      title: "Learn",
+      description: "Learn about Next.js in an interactive course with quizzes!"
+    },
+    {
+      url: "https://github.com/vercel/next.js/tree/master/examples",
+      title: "Examples",
+      description: "Discover and deploy boilerplate example Next.js projects."
+    },
+    {
+      url: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+      title: "Deploy",
+      description: "Instantly deploy your Next.js site to a public URL with Vercel."
+    },
+
+    
+  ]
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      ctas,
+    },
+  }
+}
+export default Home
